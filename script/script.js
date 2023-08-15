874aa8f068acb2e98fb0f96343b5af9dab128b98
ReDKaen <rigiy.yojik@gmail.com>
2023-08-15 14:11:40 +0300

    js

---
+++ script/script.js
@@ -0,0 +1,18 @@
+let name = "Міша";
+let admin = name;
+
+console.log(admin);
+
+
+
+let day = 7;
+let second = 24*60*60;
+let second1 = second + day;
+
+console.log("кількість секунд "+  second);
+
+
+
+let entername = prompt("enter name")
+
+console.log(entername);
